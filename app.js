8244ef2dd3c7f099495c27129b3051c59b776b57
Jaice561 <Jameson_exil@yahoo.com>
2020-05-09 00:24:24 -0500

    Starting with the front page

---
+++ app.js
@@ -0,0 +1,16 @@
+function readMore (){
+   
+    document.getElementById ("readmore").style.display="block";
+    document.getElementById ("Readmore").style.display="none";
+}
+
+function readLess (){
+    
+    document.getElementById ("readmore").style.display="none";
+    document.getElementById ("Readmore").style.display="block";
+}
+
+function initial (){
+    document.getElementById ("readmore").style.display="none"; 
+}
+
