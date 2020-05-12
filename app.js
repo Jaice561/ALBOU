647cd435d9de50735b823f381350a1b18814d2b4
Jaice561 <Jameson_exil@yahoo.com>
2020-05-12 15:33:54 -0500

    progress

--- app.js
+++ app.js
@@ -14,6 +14,29 @@ function initial (){
     document.getElementById ("readmore").style.display="none"; 
 }
 
-function shuffle() {
-    
+let suits = ["Clubs", "Diamonds","Hearts","Spades"]
+let ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+
+function getDeck() {
+    let deck = new Array();
+    for(let i = 0; i < suits.length; i++){
+        for(let x = 0; x < ranks.length; x++){
+            let card = {rank: ranks[x], suit: suits[i]}
+            deck.push (card)
+        }
+    }
+    // console.log(deck)
+    return deck;
 }
+
+shuffle();
+function shuffle() {
+    let deck = getDeck();
+    let random = Math.floor(Math.random()*52);
+    console.log(random)
+    return deck[random];
+}
+
+
+
+
